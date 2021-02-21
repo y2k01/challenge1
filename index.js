5a705e4ac432c3a6dbf1169eeb12bfe410793ed7
y2k01 <stanportableuse@gmail.com>
2021-02-22 02:42:00 +0900

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3')
+let p4 = document.getElementById('p4')
+let p5 = document.getElementById('p5')
+
+function hideUnhide(elem) {
+    elem.classList.toggle('hidden')
+}
+
+function moveLeft() {
+    document.getElementById('box-image').classList.add('moveLeft')
+}
+let q1 = document.getElementById("q1");
+q1.addEventListener("mouseover", )
